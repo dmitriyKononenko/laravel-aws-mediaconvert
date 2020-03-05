41ccc9073a2760d403b1bd20de57affbb7edd90e
dmitriyKononenko <xidjex@gmail.com>
2020-03-05 18:14:43 +0200

    Added test routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/message', function (Request $request) {
+    $postBody = file_get_contents('php://input');
+
+    Log::debug($postBody);
+    return 'test';
 });
